835f8824bc30a4284c954a03c266a6d3a36a2a5f
Eino Virtanen <eino.vir@gmail.com>
2016-08-18 03:33:04 +0300

    Initial commit

---
+++ app/src/main/java/fi/dy/einovirtanen/gymhealthcombo/MainActivity.java
@@ -0,0 +1,63 @@
+package fi.dy.einovirtanen.gymhealthcombo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    SETUP:
+     */
+    long leftCounterEpochTimestampInSeconds = 1471057200;
+    long rightCounterEpochTimestampInSeconds = 1471212000;
+
+    TextView firstCounterText;
+    TextView secondCounterText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hide status bar and notification bar:
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        getSupportActionBar().hide();
+
+        // Initialize text fields:
+        firstCounterText = (TextView) findViewById(R.id.firstCounterText);
+        secondCounterText = (TextView) findViewById(R.id.secondCounterText);
+
+        Thread t = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
+                        Thread.sleep(1000); // TODO: add: *60*60 Execute once per hour. That is precise enough.
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                firstCounterText.setText(Long.toString((System.currentTimeMillis()/1000-leftCounterEpochTimestampInSeconds)/60/60/24));
+                                secondCounterText.setText(Long.toString((System.currentTimeMillis()/1000-rightCounterEpochTimestampInSeconds)/60/60/24));
+                            }
+                        });
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        };
+
+        t.start();
+
+
+    }
+
+
+}
